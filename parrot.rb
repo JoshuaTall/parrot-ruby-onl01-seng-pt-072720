1b351b326c1cad71ba86223bca958ab17f38d307
Joshua Tall <tall.joshua1984@yahoo.com>
2020-07-28 22:38:56 +0000

    Automatically backed up by Learn

---
+++ parrot.rb
@@ -0,0 +1,20 @@
+def parrot
+  puts "Squawk"
+end
+
+  it 'should return the default phrase, "Squawk!" when called without any arguments' do
+    phrase = parrot
+    expect(phrase).to eq("Squawk!")
+  end
+
+  it 'should output the given phrase when called with an argument' do
+    expect($stdout).to receive(:puts).with("Pretty bird!")
+    parrot("Pretty bird!")
+  end
+
+  it 'should return the given phrase when called with an argument' do
+    phrase = parrot("Pretty bird!")
+
+    expect(phrase).to eq("Pretty bird!")
+  end
+end
